c42c2abbb6aa83f413908483e4bbb93ca36573c4
user <user@192.168.0.4>
2022-07-20 15:25:44 +0900

    sorry......

--- DoranDoran/src/main/java/dr/member/dao/MemberDAO.java
+++ DoranDoran/src/main/java/dr/member/dao/MemberDAO.java
@@ -233,7 +233,7 @@ public class MemberDAO {
 	}
 	
 	//프로필 사진 수정
-	public void updateMyPhoto(String photo,int mem_num)throws Exception{
+	public void updateMyPhoto(String mem_photo,int mem_num)throws Exception{
 		Connection conn=null;
 		PreparedStatement pstmt=null;
 		String sql=null;
@@ -242,11 +242,11 @@ public class MemberDAO {
 			//JDBC 수행 1,2단계 : 커넥션풀로부터 커넥션을 할당
 			conn=DBUtil.getConnection();
 			//SQL문 작성
-			sql="UPDATE zmember_detail SET photo=? WHERE mem_num=?";
+			sql="UPDATE member_detail SET photo=? WHERE mem_num=?";
 			//JDBC 수행 3단계 : PreparedStatement 객체 생성
 			pstmt=conn.prepareStatement(sql);
 			//?에 데이터 바인딩
-			pstmt.setString(1, photo);
+			pstmt.setString(1, mem_photo);
 			pstmt.setInt(2, mem_num);
 			//JDBC 수행 4단계
 			pstmt.executeUpdate();
