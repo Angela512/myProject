7aa040e35ea5c18bc0dda43bc59e1a5f2a8e49bb
user <user@192.168.0.4>
2022-07-20 14:38:10 +0900

    테이블 수정

--- DoranDoran/src/main/webapp/sql/table.sql
+++ DoranDoran/src/main/webapp/sql/table.sql
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-create table notice(
-	notice_num number not null, 
-	mem_num number not null,
-	notice_title varchar2(50) not null,
-	notice_date date not null,
-	notice_count number(5),
-	notice_content clob not null,
-	notice_file1 varchar2(150),
-	notice_file2 varchar2(150),
-	notice_file3 varchar2(150)
-);
-=======
+--trade
 create table trade(
     trade_num number not null,
 	mem_num number not null,
