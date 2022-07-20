f71047a01359f3e994e0a3209e8c0ade3577f118
user <user@192.168.0.4>
2022-07-20 16:19:17 +0900

    loginformaction

---
+++ DoranDoran/src/main/java/dr/member/action/LoginFormAction.java
@@ -0,0 +1,15 @@
+package dr.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class LoginFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return "/WEB-INF/views/member/loginForm.jsp";
+	}
+
+}
