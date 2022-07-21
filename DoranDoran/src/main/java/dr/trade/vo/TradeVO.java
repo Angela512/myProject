0640843453179f233f8020eaca8ee6105fefaac4
sk4ck <sk4ck@LAPTOP-OLMQEP30>
2022-07-21 12:38:15 +0900

    table.sql trade테이블 수정

---
+++ DoranDoran/src/main/java/dr/trade/vo/TradeVO.java
@@ -0,0 +1,20 @@
+package dr.trade.vo;
+
+import java.sql.Date;
+
+public class TradeVO {
+	private int trade_num;
+	private int mem_num;
+	private String trade_head;
+	private String trade_category;
+	private String trade_title;
+	private Date trade_date;
+	private String trade_content;
+	private int trade_price;
+	private String trade_image1;
+	private String trade_image2;
+	private String trade_image3;
+	private int trade_count;
+	private String trade_phone;
+	
+}
