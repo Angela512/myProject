38d104ea9de317430e57204f739fe5ea806a21c3
Sol <nathf1005@naver.com>
2022-08-01 10:51:37 +0900

    맛집 js 수정

--- DoranDoran/src/main/webapp/js/food.js
+++ DoranDoran/src/main/webapp/js/food.js
@@ -11,6 +11,31 @@ $(function(){
 			$('#content').val('').focus();
 			return false;
 		}
+		if($('#phone2').val().trim()==''){
+			alert('번호를 입력하세요!');
+			$('#phone2').val('').focus();
+			return false;
+		}
+		if($('#phone3').val().trim()==''){
+			alert('번호를 입력하세요!');
+			$('#phone3').val('').focus();
+			return false;
+		}
+		if($('#zipcode').val().trim()==''){
+			alert('주소를 입력하세요!');
+			$('#zipcode').val('').focus();
+			return false;
+		}
+		if($('#address1').val().trim()==''){
+			alert('주소를 입력하세요!');
+			$('#address1').val('').focus();
+			return false;
+		}
+		if($('#address2').val().trim()==''){
+			alert('주소를 입력하세요!');
+			$('#address2').val('').focus();
+			return false;
+		}
 	});
 	
 	//============== 목록 검색창 ==============//
